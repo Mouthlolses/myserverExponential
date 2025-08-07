bcac5d918cfff1508cb0a3a6bdd5c03204272005
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-08-06 21:10:37 -0300

    new_entity_salve

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,18 @@
+import cask.model.Request
 import upickle.default.*
 
 
 case class User(name: String, age: Int)
+
+case class Game(title: String, gender: String)
+
 implicit val rw: ReadWriter[User] = macroRW
 
+implicit val rwt: ReadWriter[Game] = macroRW
+
 object Main extends cask.MainRoutes:
-  var users = List.empty[User]
+  private var users = List.empty[User]
+  private var games = List.empty[Game]
 
   @cask.post("/users")
   def addUser(request: cask.Request): cask.Response[String] = {
@@ -13,6 +20,19 @@ object Main extends cask.MainRoutes:
     val user = read[User](bodyText)
     users = users :+ user
     cask.Response("User Saved", statusCode = 201)
+  }
+
+  @cask.post("/games")
+  def addGame(request: cask.Request): cask.Response[String] = {
+    val bodyText = request.text()
+    val game = read[Game](bodyText)
+    games = games :+ game
+    cask.Response("Game Saved", statusCode = 201)
+  }
+
+  @cask.get("/games")
+  def getGames(): ujson.Value = {
+    writeJs(games)
   }
 
   @cask.get("/users")
